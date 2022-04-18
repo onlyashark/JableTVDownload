d6fbd0a36418f7e0b5799aff4a1deaf89494dd35
onlyashark <54878378+onlyashark@users.noreply.github.com>
2022-04-19 07:21:27 +0800

    新增自動解碼

---
+++ encode.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+
+def ffmpeg_encode(folderpath, dirname):
+    os.chdir(folderpath)
+    try:
+        subprocess.call(['.\ffmpeg', '-i', f'{dirname}.mp4', '-c:v', 'libx264', '-b:v', '3M',
+                        '-threads', '1', '-preset', 'superfast', f'f_{dirname}.mp4'])
+        os.remove(os.path.join(folderpath, f'{dirname}.mp4'))
+        print(f"Encoded {dirname}")
+    except:
+        print(f"Fail to encode {dirname}")
